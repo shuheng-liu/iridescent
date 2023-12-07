93540c42660d5d52a84e8e546b51ed25873005b0
Shuheng Liu <Shuheng.Liu@intersystems.com>
2023-12-06 20:34:49 -0500

    docker(): write a .bat equivalent of run_docker.sh

--- run_docker.bat
+++ run_docker.bat
@@ -1,10 +1,14 @@
-#!/bin/bash
-set -e
-if [ $# -ne 2 ]; then
-  echo "Usage: ./run_docker.sh <IRIS_IMAGE>:<TAG> <IRIS_INSTANCE>"
-  exit 1
-fi
-echo "FROM $1" > Dockerfile.temporary
-cat ./docker/Dockerfile.template >> Dockerfile.temporary
-docker build -t iridescent --build-arg IRIS_INSTANCE="$2" -f Dockerfile.temporary  .
+@echo off
+setlocal
+
+if "%~2"=="" (
+  echo Usage: run_docker.bat ^<IRIS_IMAGE^>:^<TAG^> ^<IRIS_INSTANCE^>
+  exit /b 1
+)
+
+echo FROM %1 > Dockerfile.temporary
+type .\docker\Dockerfile.template >> Dockerfile.temporary
+docker build -t iridescent --build-arg IRIS_INSTANCE="%2" -f Dockerfile.temporary .
 docker container run --rm -it --name iridescent iridescent
+
+endlocal
